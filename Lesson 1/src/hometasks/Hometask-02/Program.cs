5b076d07a57e179e100b96ea0a38d5015fff3864
Kasady <44520511+buz71@users.noreply.github.com>
2021-06-29 20:57:54 +0300

    Hometask-02 DONE

--- Lesson 1/src/hometasks/Hometask-02/Program.cs
+++ Lesson 1/src/hometasks/Hometask-02/Program.cs
@@ -6,7 +6,68 @@ namespace BuzinovArtem.Hometask_02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\t\t\t\t\tПроверка счастливого билета");
+            Console.ResetColor();
+            System.Threading.Thread.Sleep(3000);
+            int Ticket;
+            begin:
+            Console.WriteLine("Введите номер билета: ");
+            if (int.TryParse(Console.ReadLine(), out Ticket))
+            {
+                if (Ticket<999999&&Ticket>99999)
+                {
+                    int backSum = 0;
+                    int temp = Ticket;
+                    for (int i = 0; i < 3; i++)
+                    {
+                        backSum = backSum + temp % 10;
+                        temp = temp / 10;
+                    }
+                    temp = Ticket/1000;
+                    int frontSum = 0;
+                    for (int i = 0; i < 3; i++)
+                    {
+                        frontSum = frontSum + temp % 10;
+                        temp = temp / 10;
+                    }
+
+                    if (frontSum==backSum)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Ура! У вас счастливый билет");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("Увы! Вам не повезло");
+                        Console.ResetColor();
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Введены не корректные значения!");
+                    Console.ResetColor();
+                    goto begin;
+                }
+
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Введены не корректные значения!");
+                Console.ResetColor();
+                goto begin;
+            }
+           
+            
+            
+
+
+
+
         }
     }
 }
