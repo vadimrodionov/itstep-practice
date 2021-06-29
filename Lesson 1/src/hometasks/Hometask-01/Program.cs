1b9dcdb536ea10175882e451aba88fd0f558b80b
Kasady <44520511+buz71@users.noreply.github.com>
2021-06-29 19:30:11 +0300

    Hometask-01 DONE

--- Lesson 1/src/hometasks/Hometask-01/Program.cs
+++ Lesson 1/src/hometasks/Hometask-01/Program.cs
@@ -6,7 +6,40 @@ namespace BuzinovArtem.Hometask_01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\t\t\t\tПрограмма считывания символов с клавиатуры");
+            System.Threading.Thread.Sleep(3000);
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            char keySymbol = '0';
+            byte counter = 0;
+            while (keySymbol!='.')
+            {
+                if (keySymbol==' ')
+                {
+                    counter++;
+                }
+                Console.WriteLine("Введите символ: ");
+                if (char.TryParse(Console.ReadLine(), out keySymbol))
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine($"Вы ввели '{keySymbol}'");
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Введены не корректные значения");
+                }
+                Console.ResetColor();
+            }
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Завершение работы программы");
+            System.Threading.Thread.Sleep(2000);
+            Console.WriteLine($"Пробел был введен {counter} раз(а)");
+            Console.ReadLine();
+
         }
     }
 }
