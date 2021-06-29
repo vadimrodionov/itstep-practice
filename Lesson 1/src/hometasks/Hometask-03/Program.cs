5261bf8fe297c407eae3222a5dc3df4e5ed848af
Kasady <44520511+buz71@users.noreply.github.com>
2021-06-29 21:26:21 +0300

    Hometask-3 DONE

--- Lesson 1/src/hometasks/Hometask-03/Program.cs
+++ Lesson 1/src/hometasks/Hometask-03/Program.cs
@@ -6,7 +6,54 @@ namespace BuzinovArtem.Hometask_03
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\t\t\t\t\tИзменение регистра символов");
+            System.Threading.Thread.Sleep(3000);
+            Console.ResetColor();
+            begin:
+            Console.WriteLine( "Введите буквенный символ");
+            char variable;
+            if (char.TryParse(Console.ReadLine(),out variable))
+            {
+                int integer=(int)variable;
+
+                if (integer>=65&&integer<=90||integer>=97&&integer<=122)
+                {
+                    if (integer >= 65 && integer <= 90)
+                    {
+                        integer += 32;
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"Символ в нижнем регистре: {(char)integer}");
+                        Console.ResetColor();
+                      
+                    }
+                    else
+                    {
+                        integer -= 32;
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine($"Символ в верхнем регистре: {(char)integer}");
+                        Console.ResetColor();
+                      
+                    }
+                    
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Введены не корректные данные");
+                    Console.ResetColor();
+                    goto begin;
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Введены не корректные данные");
+                Console.ResetColor();
+                goto begin;
+            }
+            
+            
         }
     }
 }
