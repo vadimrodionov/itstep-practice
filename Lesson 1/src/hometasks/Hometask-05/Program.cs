d7a9952b40baebca41e12169a7cbd68a5db107d5
Kasady <44520511+buz71@users.noreply.github.com>
2021-07-01 21:19:00 +0300

    Lesson 1 Complete

--- Lesson 1/src/hometasks/Hometask-05/Program.cs
+++ Lesson 1/src/hometasks/Hometask-05/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Threading;
+
 
 namespace BuzinovArtem.Hometask_05
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\t\t\t\t\tВычисление обратного числа");
+            Thread.Sleep(3000);
+            Console.ResetColor();
+            begin:
+            Console.WriteLine("Введите число: ");
+            ulong number;
+            if (ulong.TryParse(Console.ReadLine(),out number))
+            {
+                ulong inverseNumber = 0;
+                while (number%10 > 0)
+                {
+                    inverseNumber=(inverseNumber*10)+(number%10);
+                    number = number / 10;
+                }
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"Обратное число: {inverseNumber}");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Введены не корректные данные!");
+                Console.ResetColor();
+                goto begin;
+            }
+            Console.ReadLine();
         }
     }
 }
