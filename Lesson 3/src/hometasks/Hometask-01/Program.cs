f16b2d2abe5676a814ed354cdd6adb3c7d092deb
Kasady <44520511+buz71@users.noreply.github.com>
2021-07-02 19:34:20 +0300

    Folders creation in main

---
+++ Lesson 3/src/hometasks/Hometask-01/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace artem_buzinov.Hometask_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
