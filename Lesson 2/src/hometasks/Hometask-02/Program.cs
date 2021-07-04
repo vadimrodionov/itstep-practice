b1f7809cb37ef11bbf42190dc2299a0dce80f00e
Kasady <44520511+buz71@users.noreply.github.com>
2021-07-04 06:46:06 +0300

    Hometask-02 DONE

--- Lesson 2/src/hometasks/Hometask-02/Program.cs
+++ Lesson 2/src/hometasks/Hometask-02/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace artem_buzinov.Hometask_02
 {
@@ -6,7 +7,51 @@ namespace artem_buzinov.Hometask_02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\t\t\t\t\tСортировка по знаку\n");
+            Thread.Sleep(3000);
+            Console.ResetColor();
+            Console.Clear();
+
+            int[] arr = new int[10] { -1, 2, 0, -4, 0, -9, -11, 18, 0, 15 };
+            Console.WriteLine("Вывод массива\n");
+            foreach (int i in arr)
+            {
+                Console.Write(i + "\t");
+            }
+            Console.WriteLine();
+            Thread.Sleep(3000);
+            int[] arr2 = new int[arr.Length];
+            int counter = 0;
+
+            for (int i = 0,j=0; i < arr.Length; i++)
+            {
+                if (arr[i]<0)
+                {
+                    arr2[j]=arr[i];
+                    j++;
+                    counter++;
+                }
+            }
+
+            for (int i = 0, j = counter; i < arr.Length; i++)
+            {
+                if (arr[i] >= 0)
+                {
+                    arr2[j] = arr[i];
+                    j++;
+                }
+            }
+
+            Console.WriteLine("\nВывод массива после сортировки по знаку: \n");
+            Thread.Sleep(3000);
+            foreach (int i in arr2)
+            {
+                Console.Write(i + "\t");
+            }
+            Console.WriteLine(); ;
+
+
         }
     }
 }
