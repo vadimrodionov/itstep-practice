a9887cfaf0c33a9bb0c716d29193ac1f2583b1d5
Kasady <44520511+buz71@users.noreply.github.com>
2021-07-04 06:59:54 +0300

    Hometask-03 DONE

--- Lesson 2/src/hometasks/Hometask-03/Program.cs
+++ Lesson 2/src/hometasks/Hometask-03/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace artem_buzinov.Hometask_03
 {
@@ -6,7 +7,59 @@ namespace artem_buzinov.Hometask_03
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\t\t\t\t\tПоиск числа в массиве\n");
+            Thread.Sleep(3000);
+            Console.ResetColor();
+            Console.Clear();
+            Random random = new Random();
+            int[,] arr = new int[10, 10];
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    arr[i, j] = random.Next(99);
+                }
+            }
+            begin:
+            Console.WriteLine("Введите число для поиска в массиве:");
+            int number;
+            if (int.TryParse(Console.ReadLine(),out number))
+            {
+                int countNumber = 0;
+                for (int i = 0; i < 10; i++)
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        if (arr[i,j]==number)
+                        {
+                            countNumber++;
+                        }
+                    }
+                    
+                }
+                Console.WriteLine($"Введенное число встречается в программае {countNumber}, раз(a)\n");
+                Thread.Sleep(2000);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Вы ввели не корректные данные");
+                Console.ResetColor();
+                goto begin;
+            }
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(arr[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+
         }
     }
 }
