6565634e897615b10ca3085e9cd518dd43158e36
Kasady <44520511+buz71@users.noreply.github.com>
2021-07-04 12:05:15 +0300

    Hometask-05 DONE

---
+++ Lesson 2/src/hometasks/Hometask-05/Car.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace artem_buzinov.Hometask_05
+{
+    partial class Car
+    {
+        #region Fields
+        private int IdNumber;
+        private string Owner = "";
+        private string Model = "Sedan";
+        private string Color = "White";
+        private double Consumption = 0;
+        private bool EngineStarted = false;
+        private uint Speed = 0;
+        private uint MaxSpeed = 220;
+        private Random rand = new Random();
+        public static int NumberOfCars;
+        public static int LastSaleCar;
+
+        #endregion
+        #region Constructors
+        public Car()
+        {
+            NumberOfCars++;
+            IdNumber = rand.Next();
+            LastSaleCar = IdNumber;
+
+        } //default ctor
+        public Car(string Model, string Color, string Owner) 
+        {
+            this.Model = Model;
+            this.Color = Color;
+            this.Owner = Owner;
+            NumberOfCars++;
+            IdNumber = rand.Next();
+            LastSaleCar = IdNumber;
+        }
+        public Car(string Model, string Color) 
+        {
+            this.Model = Model;
+            this.Color = Color;
+            NumberOfCars++;
+            IdNumber = rand.Next();
+            LastSaleCar = IdNumber;
+        }
+        public Car(string Model)
+        {
+            this.Model = Model;
+            NumberOfCars++;
+            IdNumber = rand.Next();
+            LastSaleCar = IdNumber;
+        }
+        static Car() 
+        {
+            NumberOfCars = 0;
+            LastSaleCar = 0;
+        }
+
+        #endregion
+        #region Properties
+        public int CarIdNumber
+        {
+            get { return IdNumber; }
+        }
+        public string CarOwner { get; set; }
+        public string CarModel
+        {
+            get { return Model; }
+        }
+        public string CarColor
+        {
+            get { return Color; }
+        }
+        public double CarConsumption
+        {
+            get { return Consumption; }
+            set { Consumption = value; }
+        }
+        public bool CarIsStarted
+        {
+            get { return EngineStarted; }
+            set { EngineStarted = value; }
+        }
+        public uint CarSpeed
+        {
+            get { return Speed; }
+            set { Speed = value; }
+        }
+        public uint CarMaxSpeed
+        {
+            get { return MaxSpeed; }
+        }
+        #endregion
+        
+    }
+}
--- Lesson 2/src/hometasks/Hometask-05/Program.cs
+++ Lesson 2/src/hometasks/Hometask-05/Program.cs
@@ -6,7 +6,54 @@ namespace artem_buzinov.Hometask_05
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Car[] cars = new Car[5]; //массив элементов класса
+
+            for (int i = 0; i < cars.Length; i++)
+            {
+                cars[i] = new Car();
+            }
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Вывод массива созданных объектов");
+
+            foreach (Car car in cars)
+            {
+                car.CarInfo();
+            }
+
+            Car car1 = new Car(); //использование конструктора по умолчанию
+            Car car2 = new Car("Sedan", "Green", "Sergey"); //использование конструктора с параметрами
+            Car car3 = new Car("Universal", "Red"); //использование перегруженного конструктора с параметрами
+            Console.WriteLine("\nВывод информации об автомобиле\n");
+            car2.CarInfo(); // вывод информации об автомобиле
+            car2.StartTheCar();
+            car2.SetSpeed(100);
+            Console.WriteLine("\nВывод информации об автомобиле после изменения состояния с помощью реализованных методов\n");
+            car2.CarInfo();
+            Console.WriteLine("\nВывод информации с ипользованием статических полей\n");
+            Car.Info();
+            Console.WriteLine("\nПример использования метода с принимаемым аргументом по ссылке\n");
+            Console.WriteLine("Введите значение пройденного расстояния");
+            int distance,newDistance;
+            while (!int.TryParse(Console.ReadLine(),out distance))
+            {
+                Console.WriteLine("Неверные данные\n");
+            }
+            Console.WriteLine("Введите новое значение пройденного расстояния");
+            while (!int.TryParse(Console.ReadLine(), out newDistance))
+            {
+                Console.WriteLine("Неверные данные\n");
+            }
+            Console.ForegroundColor=ConsoleColor.Red;
+            Console.WriteLine($"\nИсходное расстояние до вызова метода: {distance}\n");
+            Car.SetDistance(ref distance, newDistance);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"\nИсходное расстояние после вызова метода: {distance}\n");
+            Console.ResetColor();
+
+
+
+
         }
     }
 }
