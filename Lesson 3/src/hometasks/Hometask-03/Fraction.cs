be10638c2ef48dfe666a77df1835df58677e9513
Kasady <44520511+buz71@users.noreply.github.com>
2021-07-05 12:46:10 +0300

    Hometask-03. Operators overload done

---
+++ Lesson 3/src/hometasks/Hometask-03/Fraction.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace artem_buzinov.Hometask_03
+{
+    class Fraction
+    {
+        #region Fields
+        private int numenator;
+        private int denominator;
+        #endregion
+        #region Properties
+        public int Numenator { get; set; }
+        public int Denominator { get; set; }
+        #endregion
+        #region Constructors
+        public Fraction() { } // default ctor
+        public Fraction(int numenator, int denominator) 
+        {
+            this.numenator = numenator;
+            this.denominator = denominator;
+        }
+        public Fraction(Fraction fraction) 
+        {
+            this.numenator = fraction.numenator;
+            this.denominator = fraction.denominator;
+        } //copy ctor
+        #endregion
+        #region Operators_overload
+        public static Fraction operator+(Fraction f1, Fraction f2) 
+        {
+            return new Fraction
+            {
+                numenator = ((f1.numenator * f2.denominator) + (f1.denominator * f2.numenator)),
+                denominator = (f1.denominator*f2.denominator)
+            };
+        }
+        public static Fraction operator -(Fraction f1, Fraction f2)
+        {
+            return new Fraction
+            {
+                numenator = ((f1.numenator * f2.denominator) - (f1.denominator * f2.numenator)),
+                denominator = (f1.denominator * f2.denominator)
+            };
+        }
+        public static Fraction operator*(Fraction f1, Fraction f2) 
+        {
+            return new Fraction
+            {
+                numenator = f1.numenator * f2.numenator,
+                denominator = f1.denominator * f2.denominator
+            };
+        }
+        public static Fraction operator/(Fraction f1, Fraction f2) 
+        {
+            return new Fraction
+            {
+                numenator = f1.numenator*f2.denominator,
+                denominator = f1.denominator*f2.numenator
+            };
+        }
+        public static bool operator==(Fraction f1, Fraction f2) 
+        {
+            return (f1.numenator * f2.denominator == f2.numenator * f1.denominator);
+        }
+        public static bool operator !=(Fraction f1, Fraction f2)
+        {
+            return (f1.numenator * f2.denominator != f2.numenator * f1.denominator);
+        }
+        public static bool operator >(Fraction f1, Fraction f2) 
+        {
+            return (f1.numenator * f2.Denominator > f2.numenator * f1.Denominator);
+        }
+        public static bool operator <(Fraction f1, Fraction f2)
+        {
+            return (f1.numenator * f2.Denominator < f2.numenator * f1.Denominator);
+        }
+        public static bool operator >=(Fraction f1, Fraction f2)
+        {
+            return (f1.numenator * f2.Denominator >= f2.numenator * f1.Denominator);
+        }
+        public static bool operator <=(Fraction f1, Fraction f2)
+        {
+            return (f1.numenator * f2.Denominator > f2.numenator * f1.Denominator);
+        }
+        public static bool operator true(Fraction f1) 
+        {
+            if (f1.numenator < f1.denominator)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+           
+        }
+        public static bool operator false(Fraction f1)
+        {
+            if (f1.numenator > f1.denominator)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        #endregion
+        #region Methods
+        public void Print() 
+        {
+            Console.WriteLine($"{this.numenator}/{this.denominator}\n");
+        }
+        #endregion
+
+
+    }
+}
--- Lesson 3/src/hometasks/Hometask-03/Program.cs
+++ Lesson 3/src/hometasks/Hometask-03/Program.cs
@@ -6,7 +6,23 @@ namespace artem_buzinov.Hometask_03
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Fraction f1 = new Fraction(2,3);
+            Fraction f2 = new Fraction(3,4);
+            Fraction f3 = new Fraction();
+            f3 = f1 - f2;
+            f1.Print();
+            f2.Print();
+            f3.Print();
+
+            if (f1>f2)
+            {
+                Console.WriteLine("F1 больше");
+            }
+            else
+            {
+                Console.WriteLine("F2 больше");
+            }
+
         }
     }
 }
