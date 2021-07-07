85e427b9ab10785e4a6338c139f28e6e8330faf8
Plot_4vfo@stud.chel.itstep.org <Plot_4vfo@stud.chel.itstep.org>
2021-07-08 00:34:14 +0500

    Created projects for lesson 1 tasks

---
+++ 01/src/hometasks/Hometask-01/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ElenaPlotnikova.Hometask_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
