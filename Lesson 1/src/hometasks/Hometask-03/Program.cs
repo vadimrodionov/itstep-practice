547f1c59676378c7abf4fb7bad6b68adfd45108f
Kasady <pupcok@yandex.ru>
2021-07-08 21:59:17 +0300

    Hometask-03 Code review DONE

--- Lesson 1/src/hometasks/Hometask-03/Program.cs
+++ Lesson 1/src/hometasks/Hometask-03/Program.cs
@@ -3,58 +3,52 @@ using System.Threading;
 
 namespace BuzinovArtem.Hometask_03
 {
+    /// <summary>
+    /// Провел работу над ошибками. Удалил goto, вынес цветную консоль в отдельный метод
+    /// </summary>
     class Program
     {
+        public static void WriteColoredLine(ConsoleColor color, string message)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine($"{message}");
+            Console.ResetColor();
+        }
         static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("\t\t\t\t\tИзменение регистра символов");
+            WriteColoredLine(ConsoleColor.Red, "\t\t\t\t\tИзменение регистра символов");
             Thread.Sleep(3000);
             Console.ResetColor();
-            begin:
-            Console.WriteLine( "Введите буквенный символ");
             char variable;
-            if (char.TryParse(Console.ReadLine(),out variable))
+            do
             {
-                int integer=(int)variable;
+                Console.WriteLine("Введите буквенный символ");
 
-                if (integer>=65&&integer<=90||integer>=97&&integer<=122)
-                {
-                    if (integer >= 65 && integer <= 90)
-                    {
-                        integer += 32;
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine($"Символ в нижнем регистре: {(char)integer}");
-                        Console.ResetColor();
-                      
-                    }
-                    else
-                    {
-                        integer -= 32;
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.WriteLine($"Символ в верхнем регистре: {(char)integer}");
-                        Console.ResetColor();
-                      
-                    }
-                    
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Введены не корректные данные");
-                    Console.ResetColor();
-                    goto begin;
-                }
+
+            } while (!char.TryParse(Console.ReadLine(), out variable));
+
+            int integer = (int)variable;
+
+            if (integer >= 65 && integer <= 90)
+            {
+                integer += 32;
+                WriteColoredLine(ConsoleColor.Green, $"Символ в нижнем регистре: {(char)integer}");
+            }
+            else if (integer >= 97 && integer <= 122)
+            {
+                integer -= 32;
+                WriteColoredLine(ConsoleColor.Green, $"Символ в верхнем регистре: {(char)integer}");
             }
             else
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Введены не корректные данные");
-                Console.ResetColor();
-                goto begin;
+                WriteColoredLine(ConsoleColor.Red, "Вы ввели не буквенный символ!");
             }
-            
-            
+
         }
     }
+
+
 }
+
+
