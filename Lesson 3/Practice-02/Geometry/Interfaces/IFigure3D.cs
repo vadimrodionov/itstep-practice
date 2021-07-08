40d0243c8a5c09d2562b97448872b44cce5c6bf9
Kasady <pupcok@yandex.ru>
2021-07-08 18:57:41 +0300

    Practice-02

---
+++ Lesson 3/Practice-02/Geometry/Interfaces/IFigure3D.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_02.Geometry.Interfaces
+{
+    interface IFigure3D
+    {
+        public double Volume { get;}
+        double SurfaceArea { get;}
+
+        
+    }
+}
