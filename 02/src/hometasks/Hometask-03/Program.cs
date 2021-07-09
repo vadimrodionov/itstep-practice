bbae20d48ec7c685396f964aa091c35f64d00e41
Bogdan <udincevb@gmail.com>
2021-07-09 22:39:06 +0300

    Comleted hometask for lesson 2

---
+++ 02/src/hometasks/Hometask-03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace UdincevBogdan.Hometask_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 12, 1, 12, 12, 5, 6, 5, 7};
+            foreach(int i in array)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.Write("Введи число: ");
+            int num = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for(int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == num) sum++;
+            }
+            Console.WriteLine($"Всего совпадений: {sum}");
+        }
+    }
+}
---
+++ 02/src/hometasks/Hometask-05/AddForceToRocket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdincevBogdan.Hometask_05
+{
+    partial class Rocket
+    {
+        public void AddForceToRocket(ref Rocket rocket, double Force)
+        {
+            rocket.SpecificImpulse += Force;
+        }
+    }
+}
