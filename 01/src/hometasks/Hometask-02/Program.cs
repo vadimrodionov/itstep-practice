1ee026e6d7b7d4902a7592b23c89ee8f32dbadeb
Bogdan <udincevb@gmail.com>
2021-07-09 22:24:11 +0300

    Completed hometasks for lesson 1

---
+++ 01/src/hometasks/Hometask-02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BogdanUdincev.Hometask_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] bil = new int[6];
+            int sum1 = 0, sum2 = 0;
+            Console.WriteLine("Введите 6 цифр билета:");
+            for (int i = 0; i < bil.Length; i++)
+            {
+                Console.Write($"{i + 1}. ");
+                bil[i] = (Console.ReadKey().KeyChar - '0');
+                Console.WriteLine();
+            }
+            for (int i = 0; i < bil.Length - 3; i++) sum1 += bil[i];
+            for (int i = 3; i < bil.Length; i++) sum2 += bil[i];
+            Console.WriteLine("Билет счастливый: " + (sum1 == sum2 ? true : false));
+        }
+    }
+}
---
+++ 01/src/hometasks/Hometask-03/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BogdanUdincev.Hometask_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string str = Console.ReadLine();
+            var isUpperCase = str.ToUpper() == str;
+            if (isUpperCase == false) str = str.ToUpper();
+            else if (isUpperCase == true) str = str.ToLower();
+            Console.WriteLine(str);
+        }
+    }
+}
