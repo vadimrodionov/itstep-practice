773b64185c490d7e8fec6ad33e80cfbb52e059d8
Kasady <pupcok@yandex.ru>
2021-07-09 22:14:58 +0300

    Hometask-04 delete goto operator

--- Lesson 2/src/hometasks/Hometask-04/Program.cs
+++ Lesson 2/src/hometasks/Hometask-04/Program.cs
@@ -12,7 +12,6 @@ namespace artem_buzinov.Hometask_04
             Thread.Sleep(3000);
             Console.ResetColor();
             Console.Clear();
-
             #region array creation and fill
             Random random = new Random();
             int[,] arr = new int[10, 10];
@@ -24,7 +23,6 @@ namespace artem_buzinov.Hometask_04
                 }
             }
             #endregion
-
             #region вывод базового массива
             for (int i = 0; i < 10; i++)
             {
@@ -35,85 +33,40 @@ namespace artem_buzinov.Hometask_04
                 Console.WriteLine();
             }
             #endregion
-
             Console.WriteLine("\nВведите номер столбцов для замены\n");
             Thread.Sleep(2000);
-        begin_firstRow:
-            Console.WriteLine("Первый столбец: ");
             int firstRow;
             int secondRow;
-            if (int.TryParse(Console.ReadLine(),out firstRow))
+            do
             {
-                if (firstRow>=10||firstRow<0)
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Неверный индекс. " +
-                                      "Индекс должен быть не больше 10 и не меньше 0");
-                    Console.ResetColor();
-                    goto begin_firstRow;
-                }
-                begin_secondRow:
+                Console.WriteLine("Первый столбец: ");
+            }
+            while (!int.TryParse(Console.ReadLine(), out firstRow) || firstRow >= 10 && firstRow > 0);
+
+            do
+            {
                 Console.WriteLine("Второй столбец: ");
-                if (int.TryParse(Console.ReadLine(), out secondRow))
-                {
-                    if (secondRow >= 10 ||secondRow < 0)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Неверный индекс. " +
-                                          "Индекс должен быть не больше 10 и не меньше 0");
-                        Console.ResetColor();
-                        goto begin_secondRow;
-                    }
-
-                    if (secondRow==firstRow)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Индексы не могут быть равны");
-                        Console.ResetColor();
-                        goto begin_secondRow;
-                    }
-                    for (int i = 0; i < 10; i++)
-                    {
-                        int buffer = arr[i, firstRow];
-                        arr[i, firstRow] = arr[i, secondRow];
-                        arr[i, secondRow] = buffer;
-                    }
-
-                    Console.WriteLine("Массив после замены столбцов:\n");
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    for (int i = 0; i < 10; i++)
-                    {
-                        for (int j = 0; j < 10; j++)
-                        {
-                            Console.Write(arr[i, j] + "\t");
-                        }
-                        Console.WriteLine();
-                    }
-
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Вы ввели не корректные данные");
-                    Console.ResetColor();
-                    goto begin_secondRow;
-                }
-
             }
-            else
+            while (!int.TryParse(Console.ReadLine(), out secondRow) || secondRow >= 10 && secondRow > 0);
+
+            for (int i = 0; i < 10; i++)
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Вы ввели не корректные данные");
-                Console.ResetColor();
-                goto begin_firstRow;
-
+                int buffer = arr[i, firstRow];
+                arr[i, firstRow] = arr[i, secondRow];
+                arr[i, secondRow] = buffer;
+            }
+            Console.WriteLine("Массив после замены столбцов:\n");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(arr[i, j] + "\t");
+                }
+                Console.WriteLine();
             }
 
-
-
-
-
-            Console.ReadLine();
         }
     }
 }
+
