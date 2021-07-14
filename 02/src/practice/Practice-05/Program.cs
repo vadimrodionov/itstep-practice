e4c040d729702153b0845f0fce5a3836c80ba384
Plot_4vfo@stud.chel.itstep.org <Plot_4vfo@stud.chel.itstep.org>
2021-07-15 01:28:50 +0500

    Создан шаблон

---
+++ 02/src/practice/Practice-05/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ElenaPlotnikova.Practice_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
