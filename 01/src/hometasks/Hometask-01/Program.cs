e6113b39ba25f6697a4448667cdc74626dcc8f2b
Plot_4vfo@stud.chel.itstep.org <Plot_4vfo@stud.chel.itstep.org>
2021-07-15 00:36:38 +0500

    DONE

--- 01/src/hometasks/Hometask-01/Program.cs
+++ 01/src/hometasks/Hometask-01/Program.cs
@@ -6,7 +6,17 @@ namespace ElenaPlotnikova.Hometask_01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите символ: ");
+            char symbol;
+            int count = 0;
+            do
+            {
+                symbol = (char)Console.ReadKey().KeyChar;
+                if (symbol == ' ')
+                    count++;
+            }
+            while (symbol != '.');
+            Console.WriteLine("\nКоличество введенных пробелов: " + count);
         }
     }
 }
