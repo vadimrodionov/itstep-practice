18e88f29b6573e3cea8c2806c5bed64ebac749a6
Kasady <pupcok@yandex.ru>
2021-07-16 16:20:56 +0300

    Add classes: Card,Player, Game, Show; Add interfaces IGame, IGameProcess

---
+++ Lesson 5/src/hometasks/Hometask-01/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace artem_buzinov.Hometask_01
+{
+    class Card <T>
+    {
+        string suit;
+        T value;
+        string Suit { get; }
+        T Value { get; }
+        public Card(string suit, T value) 
+        {
+            this.suit = suit;
+            this.value = value;
+        }
+    }
+}
---
+++ Lesson 5/src/hometasks/Hometask-01/IGameProcess.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace artem_buzinov.Hometask_01
+{
+    interface IGameProcess
+    {
+        void PutCard();
+        void CheckBiggestCard();
+        void DropOutPlayer();
+    }
+}
