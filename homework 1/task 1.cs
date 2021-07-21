79a9467c0f5ab313010eed1dfb5e11221a27d4da
Miha_dr72@itstepru.onmicrosoft.com <Miha_dr72@itstepru.onmicrosoft.com>
2021-07-21 21:47:47 +0300

    task 1 from homework 1 done

---
+++ homework 1/task 1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a programm that'll help you count amount of symbols untill the symbol .")
+            Console.WriteLine("Enter some symbols: ");
+            char input;
+            int spaceCount = 0;
+            do
+            {
+                input = (char)Console.ReadKey(); // по разному пытался, не понимаю как правильно это сделать
+                if (input == ' ')
+                    spaceCount++;
+            }
+            while (input != '.');
+
+            Console.WriteLine("Quantity of spaces: " + spaceCount);
+
+        }
+    }
+}
