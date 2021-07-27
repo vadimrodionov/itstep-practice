e578a3324e2bb5700032fa3a096d5ea37a87346e
Kasady <pupcok@yandex.ru>
2021-07-27 18:35:36 +0300

    Create actors and locations

---
+++ Lesson 6/src/hometasks/Hometask-01/Actors/DeliveryServiceTruck.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace artem_buzinov.Hometask_01.Actors
+{
+    class DeliveryServiceTruck
+    {
+    }
+}
