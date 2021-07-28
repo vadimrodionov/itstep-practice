0ca46ac490bf33b36fdfe4ad1a9932d4ad709085
Kasady <pupcok@yandex.ru>
2021-07-28 06:36:57 +0300

    First Event & Delegate

--- Lesson 6/src/hometasks/Hometask-01/Actors/Customer.cs
+++ Lesson 6/src/hometasks/Hometask-01/Actors/Customer.cs
@@ -8,5 +8,32 @@ namespace artem_buzinov.Hometask_01.Actors
 {
     class Customer
     {
+        #region FIELDS
+        private string name;
+        #endregion
+        public Customer(string name)
+        {
+            this.name = name;
+            _comeIntoCentre comeInto = EComeIntoCentre();
+        }
+        #region PROPERTIES
+        public string Name
+        {
+            get { return name; }
+        }
+        #endregion
+        #region DELEGATES
+        public delegate void _comeIntoCentre();
+        #endregion
+        #region METHODS
+        
+        #endregion
+        #region EVENTS
+        public event _comeIntoCentre EComeIntoCentre;
+        #endregion
+        #region EVENT_HANDLERS
+        
+        #endregion
+
     }
 }
--- Lesson 6/src/hometasks/Hometask-01/Program.cs
+++ Lesson 6/src/hometasks/Hometask-01/Program.cs
@@ -1,12 +1,18 @@
 using System;
+using artem_buzinov.Hometask_01.Actors;
 
 namespace artem_buzinov.Hometask_01
 {
     class Program
     {
+        public void ComeMessage()
+        {
+            Console.WriteLine("Клиент прибыл");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Customer customer = new Customer("Jack");
+            
         }
     }
 }
