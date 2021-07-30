6e76ba42bfa21c560989cd8c6d273973a5034f93
Kasady <pupcok@yandex.ru>
2021-07-30 18:17:34 +0300

    New Programm Arhitecture begin

--- Lesson 6/src/hometasks/Hometask-01/Program.cs
+++ Lesson 6/src/hometasks/Hometask-01/Program.cs
@@ -9,33 +9,7 @@ namespace artem_buzinov.Hometask_01
     {
         static void Main(string[] args)
         {
-            PlaceOfReceipt OZON = new PlaceOfReceipt();
-            Employee employee = new Employee("Jack");
-            DeliveryServiceTruck truck = new DeliveryServiceTruck();
-            Console.WriteLine("Начало истории");
-            Thread.Sleep(2000);
-            OZON.CentreStatus_Call();
-            Thread.Sleep(2000);
-            Console.WriteLine(employee.Status); 
-            employee.GoToCentre();
-            Console.WriteLine(employee.Status);
-            Thread.Sleep(2000);
-            truck.TruckCometoCentre += employee.GoToOffload;
-            Thread.Sleep(2000);
-            truck.ComeToCentre_Call();
-            Thread.Sleep(2000);
-            Console.WriteLine(employee.Status);
-            employee.OffloadFinished += employee.GoToCentre;
-            employee.OffLoadFinished_Call();
-            Thread.Sleep(2000);
-            Console.WriteLine(employee.Status);
-
-
-
-
-
-
-
+       
         }
     }
 }
