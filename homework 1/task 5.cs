1466ecf11e1b540dbb68df4544b6e4b86967e6d2
Miha_dr72@itstepru.onmicrosoft.com <Miha_dr72@itstepru.onmicrosoft.com>
2021-08-04 19:58:06 +0300

    task 5 done

---
+++ homework 1/task 5.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		String res = "";
+		int obratnoe = 0;
+		for (; n > 9;)
+		{
+			res += (n % 10).ToString();
+			n = n / 10;
+		}
+		res += n.ToString();
+		obratnoe = Convert.ToInt(res);
+		Console.WriteLine(res);
+		Console.ReadLine();
+	}
+}
